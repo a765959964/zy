98fd7ec975ddf0262d42b06bcba3253ba2fc757e
Your Name <you@example.com>
2020-02-04 00:30:03 +0800

    商户提款

--- zy-admin-web/src/main/java/com/ruoyi/web/controller/zy/MerchantWithdrawalController.java
+++ zy-admin-web/src/main/java/com/ruoyi/web/controller/zy/MerchantWithdrawalController.java
@@ -1,47 +1,50 @@
 package com.ruoyi.web.controller.zy;
 
-import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.core.page.TableDataInfo;
-import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.zy.domain.MerchantWithdrawal;
-import com.ruoyi.zy.service.IMerchantWithdrawalService;
+import java.util.List;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.zy.domain.MerchantWithdrawal;
+import com.ruoyi.zy.service.IMerchantWithdrawalService;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.page.TableDataInfo;
 
 /**
  * 提款记录Controller
  * 
  * @author zy
- * @date 2019-11-20
+ * @date 2020-02-04
  */
 @Controller
-@RequestMapping("/zy/merchantWithdrawal")
+@RequestMapping("/zy/merchantwithdrawal")
 public class MerchantWithdrawalController extends BaseController
 {
-    private String prefix = "zy/merchantWithdrawal";
+    private String prefix = "zy/merchantwithdrawal";
 
     @Autowired
     private IMerchantWithdrawalService merchantWithdrawalService;
 
-    @RequiresPermissions("zy:merchantWithdrawal:view")
+    @RequiresPermissions("zy:merchantwithdrawal:view")
     @GetMapping()
-    public String merchantWithdrawal()
+    public String merchantwithdrawal()
     {
-        return prefix + "/merchantWithdrawal";
+        return prefix + "/merchantwithdrawal";
     }
 
     /**
      * 查询提款记录列表
      */
-    @RequiresPermissions("zy:merchantWithdrawal:list")
+    @RequiresPermissions("zy:merchantwithdrawal:list")
     @PostMapping("/list")
     @ResponseBody
     public TableDataInfo list(MerchantWithdrawal merchantWithdrawal)
@@ -54,7 +57,7 @@ public class MerchantWithdrawalController extends BaseController
     /**
      * 导出提款记录列表
      */
-    @RequiresPermissions("zy:merchantWithdrawal:export")
+    @RequiresPermissions("zy:merchantwithdrawal:export")
     @Log(title = "提款记录", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
     @ResponseBody
@@ -62,7 +65,7 @@ public class MerchantWithdrawalController extends BaseController
     {
         List<MerchantWithdrawal> list = merchantWithdrawalService.selectMerchantWithdrawalList(merchantWithdrawal);
         ExcelUtil<MerchantWithdrawal> util = new ExcelUtil<MerchantWithdrawal>(MerchantWithdrawal.class);
-        return util.exportExcel(list, "merchantWithdrawal");
+        return util.exportExcel(list, "merchantwithdrawal");
     }
 
     /**
@@ -77,7 +80,7 @@ public class MerchantWithdrawalController extends BaseController
     /**
      * 新增保存提款记录
      */
-    @RequiresPermissions("zy:merchantWithdrawal:add")
+    @RequiresPermissions("zy:merchantwithdrawal:add")
     @Log(title = "提款记录", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
@@ -100,7 +103,7 @@ public class MerchantWithdrawalController extends BaseController
     /**
      * 修改保存提款记录
      */
-    @RequiresPermissions("zy:merchantWithdrawal:edit")
+    @RequiresPermissions("zy:merchantwithdrawal:edit")
     @Log(title = "提款记录", businessType = BusinessType.UPDATE)
     @PostMapping("/edit")
     @ResponseBody
@@ -112,7 +115,7 @@ public class MerchantWithdrawalController extends BaseController
     /**
      * 删除提款记录
      */
-    @RequiresPermissions("zy:merchantWithdrawal:remove")
+    @RequiresPermissions("zy:merchantwithdrawal:remove")
     @Log(title = "提款记录", businessType = BusinessType.DELETE)
     @PostMapping( "/remove")
     @ResponseBody
